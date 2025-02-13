d0581e335f435807393b2a420c1371e618080b3f
kashishkhatter <kashish.khatter04@gmail.com>
2025-02-13 20:46:51 +0530

    Updated header UI and contact us email section

--- app/_components/Contect.jsx
+++ app/_components/Contect.jsx
@@ -1,15 +1,14 @@
 "use client";
-import { db } from "@/utils/db";
-import { Newsletter } from "@/utils/schema";
 import { LoaderCircle } from "lucide-react";
-import moment from "moment";
 import React, { useState } from "react";
 import { toast } from "sonner";
+import emailjs from "@emailjs/browser";
 
 const Contect = () => {
   const handleInputChange = (setState) => (e) => {
     setState(e.target.value);
   };
+
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
   const [message, setMessage] = useState("");
@@ -18,36 +17,37 @@ const Contect = () => {
   const onSubmit = async (e) => {
     e.preventDefault();
 
-    console.log(name, email, message);
-
-    if (name && email && message) {
-      setLoading(true);
-      try {
-        const resp = await db.insert(Newsletter).values({
-          newName: name,
-          newEmail: email,
-          newMessage: message,
-          createdAt: moment().format("YYYY-MM-DD"),
-        });
-
-        if (resp) {
-          toast("User Response recorded successfully");
-          setName("");
-          setEmail("");
-          setMessage("");
-        } else {
-          toast("Error recording response");
-        }
-      } catch (error) {
-        console.error(error);
-        toast("Error recording response");
-      } finally {
-        setLoading(false);
-      }
-    } else {
+    if (!name || !email || !message) {
       toast("No data entered");
+      return;
+    }
+
+    setLoading(true);
+
+    try {
+      const response = await emailjs.send(
+        "service_pis0kap", // Replace with your EmailJS Service ID
+        "template_m9g8men", // Replace with your EmailJS Template ID
+        { name, email, message },
+        "lCgC3K0trGwlsw2Rb" // Replace with your EmailJS User ID (Public Key)
+      );
+
+      if (response.status === 200) {
+        toast("Message sent successfully");
+        setName("");
+        setEmail("");
+        setMessage("");
+      } else {
+        toast("Failed to send message");
+      }
+    } catch (error) {
+      console.error(error);
+      toast("Error sending message");
+    } finally {
+      setLoading(false);
     }
   };
+
   return (
     <div className="container mx-auto text-center">
       <h2 className="text-4xl font-bold text-gray-800">Get In Touch</h2>
